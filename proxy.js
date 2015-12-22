2a74c8ef391e2e5224b40f368e64f132a48b2ad4
Jin Cui <cuijin92@gmail.com>
2015-12-21 23:08:46 -0700

    proxy server choosing mechanism change to random

--- proxy.js
+++ proxy.js
@@ -5,17 +5,35 @@ function setup_proxy(server_list){
   setup_pac_data(server_list);
 }
 
+function generateRandomIdx(length){
+  return Math.floor((Math.random()*length)+1);
+}
+
 function setup_pac_data(server_list){
   console.log("enter setup_pac_data func");
   // from http://pac.unblockcn.com
   //var pac_server_addr = 'PROXY 203.88.160.145:80; ';// = 'PROXY 101.226.249.237:80; '+'DIRECT';
   var pac_server_addr = '';
-  for(var i = 0; i < 4; i++)
+  var index = [];
+  var ridx = generateRandomIdx(server_list.length);
+  index.push(ridx);
+  var tmp = generateRandomIdx(server_list.length);
+  if(ridx != tmp){
+    ridx = tmp;
+    index.push(ridx);
+  }
+  tmp = generateRandomIdx(server_list.length);
+  if(ridx != tmp){
+    ridx = tmp;
+    index.push(ridx);
+  }
+
+  for(var i = 0; i < index.length; i++)
   {
     pac_server_addr += 'PROXY ' 
-                       + server_list[i].ip 
+                       + server_list[index[i]].ip 
                        + ':' 
-                       + server_list[i].port 
+                       + server_list[index[i]].port 
                        + '; ';
   }
   pac_server_addr += 'DIRECT';
